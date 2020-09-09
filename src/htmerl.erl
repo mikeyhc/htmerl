56183af8e2f4412bd9f98ec8dc5c4321c79871df
mikeyhc <mikeyhc@atmosia.net>
2020-09-09 13:47:29 -0400

    added attributes and link tag

--- src/htmerl.erl
+++ src/htmerl.erl
@@ -8,8 +8,12 @@
          make_document/2,
          % general tags
          tag/2, tag/3,
+         % attribute functions
+         attribute/2, add_attribute/2, add_attributes/2,
          % standard tags
-         body/1, h1/1, head/1
+         body/1, h1/1, head/1, link/0,
+         % standard attributes
+         rel/1, src/1, type/1
         ]).
 
 %%%%%%%%%%%%%%%%%%%%%%%%
@@ -25,18 +29,33 @@ render(#document{doctype=DT, head=H, body=B}) ->
 -spec render_tag(no_html | tag()) -> binary().
 render_tag(no_html) -> <<>>;
 render_tag(B) when is_binary(B) -> B;
-render_tag(#tag{name=N, content=[], can_empty=true}) ->
-    <<"<", N/binary, "\n/>">>;
-render_tag(#tag{name=N, content=no_html, can_empty=true}) ->
-    <<"<", N/binary, "\n/>">>;
-render_tag(#tag{name=N, content=C}) ->
+render_tag(T=#tag{name=N, content=[], can_empty=true}) ->
+    Attrs = render_attributes(T#tag.attributes),
+    <<"<", N/binary, " ", Attrs/binary, " \n/>">>;
+render_tag(T=#tag{name=N, content=C}) ->
     Content = render_tags(C),
-    <<"<", N/binary, "\n>", Content/binary, "</", N/binary, "\n>">>.
+    Attrs = render_attributes(T#tag.attributes),
+    <<"<", N/binary, " ", Attrs/binary, "\n>", Content/binary,
+      "</", N/binary, "\n>">>.
+
+-spec bin_join(binary(), binary()) -> binary().
+bin_join(X, Y) -> <<X/binary, Y/binary>>.
+
+-spec bin_join_space(binary(), binary()) -> binary().
+bin_join_space(X, Y) -> <<X/binary, " ", Y/binary>>.
 
 -spec render_tags([tag()]) -> binary().
 render_tags(L) ->
-    lists:foldl(fun(X, A) -> <<X/binary, A/binary>> end, <<>>,
-                lists:map(fun render_tag/1, L)).
+    lists:foldl(fun bin_join/2, <<>>, lists:map(fun render_tag/1, L)).
+
+-spec render_attributes([attribute()]) -> binary().
+render_attributes(L) ->
+    lists:foldl(fun bin_join_space/2, <<>>,
+                lists:map(fun render_attribute/1, L)).
+
+-spec render_attribute(attribute()) -> binary().
+render_attribute(#attribute{name=N, value=V}) ->
+    <<N/binary, "=\'", V/binary, "\'">>.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% Document Functions %%%
@@ -44,6 +63,21 @@ render_tags(L) ->
 
 -spec make_document(tag() | [tag()], html() | [html()]) -> document().
 make_document(Head, Body) -> #document{head=Head, body=Body}.
+
+%%%%%%%%%%%%%%%%%%%%%
+%%% Tag Functions %%%
+%%%%%%%%%%%%%%%%%%%%%
+
+flip(F) -> fun(X, Y) -> F(Y, X) end.
+
+-spec attribute(binary(), binary()) -> attribute().
+attribute(Name, Value) -> #attribute{name=Name, value=Value}.
+
+-spec add_attribute(tag(), attribute()) -> tag().
+add_attribute(T, A) -> T#tag{attributes=[A|T#tag.attributes]}.
+
+-spec add_attributes(tag(), [attribute()]) -> tag().
+add_attributes(T, L) -> lists:foldl(flip(fun add_attribute/2), T, L).
 
 %%%%%%%%%%%%%%%%%%%%
 %%% General Tags %%%
@@ -56,6 +90,7 @@ tag(Name, Value) -> tag(Name, Value, []).
 tag(Name, Value, Opts) when is_list(Value) ->
     PreTag = lists:foldl(fun handle_tag_opts/2, #tag{}, Opts),
     PreTag#tag{name=Name, content=Value};
+tag(Name, no_html, Opts) -> tag(Name, [], Opts);
 tag(Name, Value, Opts) -> tag(Name, [Value], Opts).
 
 -spec handle_tag_opts(tag_option(), tag()) -> tag().
@@ -74,3 +109,19 @@ h1(Value) -> tag(<<"h1">>, Value).
 
 -spec head(tag() | [tag()]) -> tag().
 head(Tags) -> tag(<<"head">>, Tags, [{can_empty, false}]).
+
+-spec link() -> tag().
+link() -> tag(<<"link">>, no_html).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% Standard Attributes %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+-spec rel(binary()) -> tag().
+rel(V) -> attribute(<<"rel">>, V).
+
+-spec src(binary()) -> tag().
+src(V) -> attribute(<<"src">>, V).
+
+-spec type(binary()) -> tag().
+type(V) -> attribute(<<"type">>, V).
